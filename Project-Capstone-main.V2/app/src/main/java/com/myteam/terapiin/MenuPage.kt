b94cf88030737d3efacb61c7a69d37558bfbfb6c
musaabid <37520180+musaabid@users.noreply.github.com>
2022-06-10 15:30:21 +0700

    Update Icon Apps
    revision for icon to be the new one

---
+++ Project-Capstone-main.V2/app/src/main/java/com/myteam/terapiin/MenuPage.kt
@@ -0,0 +1,26 @@
+package com.myteam.terapiin
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toast
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.NavigationUI.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import kotlinx.android.synthetic.main.fragment_home.*
+
+
+class MenuPage : AppCompatActivity() {
+    private lateinit var navController: NavController
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.menu_page)
+
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.mainContainer) as NavHostFragment
+        navController = navHostFragment.navController
+        val bottomNavigationView = findViewById<BottomNavigationView>(R.id.bottom_navigation)
+        setupWithNavController(bottomNavigationView, navController)
+    }
+}
---
+++ Project-Capstone-main.V2/app/src/main/java/com/myteam/terapiin/TerapiMenu.kt
@@ -0,0 +1,51 @@
+package com.myteam.terapiin
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageView
+import android.widget.Toast
+import com.myteam.terapiin.databinding.ActivityTerapiMenuBinding
+
+class TerapiMenu : AppCompatActivity() {
+    private lateinit var binding: ActivityTerapiMenuBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityTerapiMenuBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.camOpenMouth.setOnClickListener{
+            val intent = Intent(this@TerapiMenu,CameraPage::class.java)
+            intent.putExtra("openmouth",1)
+            startActivity(intent)
+        }
+        binding.camPuffCheek.setOnClickListener{
+            val intent = Intent(this@TerapiMenu,CameraPage::class.java)
+            intent.putExtra("puffcheek",2)
+            startActivity(intent)
+        }
+        binding.camShowTeeth.setOnClickListener{
+            val intent = Intent(this@TerapiMenu,CameraPage::class.java)
+            intent.putExtra("showteeth",3)
+            startActivity(intent)
+        }
+        binding.camSmile.setOnClickListener{
+            val intent = Intent(this@TerapiMenu,CameraPage::class.java)
+            intent.putExtra("smile",4)
+            startActivity(intent)
+        }
+        binding.camSneer.setOnClickListener{
+            val intent = Intent(this@TerapiMenu,CameraPage::class.java)
+            intent.putExtra("sneer",5)
+            startActivity(intent)
+        }
+
+        val buttonKembali : ImageView = findViewById(R.id.backButton)
+        buttonKembali.setOnClickListener{
+            startActivity(Intent(this@TerapiMenu,MenuPage::class.java))
+        }
+    }
+
+}
